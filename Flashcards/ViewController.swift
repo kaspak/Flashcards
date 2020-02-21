859b893f3a31eab52dc8cbb3efe543d6cf94bdf8
kaspak <kpak3@dons.usfca.edu>
2020-02-21 00:18:24 -0800

    finished optional features

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backLabel: UIButton!
+    @IBOutlet weak var frontLabel: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func didTapOnFrontCard(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
+    @IBAction func didTapOnBackCard(_ sender: Any) {
+        frontLabel.isHidden = false
+    }
 }
-
